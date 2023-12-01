0deafd59e70a1ade3f4879905b73db78b31245e7
Jensen Omega <omega90ej@gmail.com>
2023-11-30 20:57:58 -0800

    still working on e17

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -1,9 +1,9 @@
 // see e16.md
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
-export function find<Type> (array: Type[], callback: (item: Type, index: number, array: Type[]) => boolean): Type | undefined {
+export function find<T> (array: (T | undefined)[], callback: (item: T | undefined, index: number, array: (T | undefined)[]) => boolean): T | undefined {
    
-   for(let i = 0; i< array.length; i++) {
+   for(let i:number = 0; i< array.length; i++) {
       if(callback(array[i], i, array)) {
          return array[i]
       }
--- exercises/e17.ts
+++ exercises/e17.ts
@@ -1,3 +1,31 @@
-export const minBy = (array, cb) => {};
+export const minBy = <T>(array: (T[] | undefined), cb:(item: T) => number): T | undefined => {
+   
+   if(!array || array.length === 0) {
+      return undefined;
+   } else {
+      let firstArray: T | undefined = array[0];
+      for (let i = 0; i < array.length; i++) {
+         if (firstArray !== undefined  && cb(firstArray) > cb(array[i] as T)) {
+            firstArray = array[i]
+         }
+      }
+      return firstArray;
+   }
 
-export function maxBy(array, cb) {}
+};
+
+export function maxBy <T> (array: (T[] | undefined)[], cb:(item: T) => number): T | undefined {
+   if(!array || array.length === 0) {
+      return undefined
+   } else {
+      let firstArray: T | undefined = array[0] && array[0][0];
+      for (let i = 0; i < array.length; i++) {
+         if(firstArray !== undefined && cb(firstArray as T) < cb(array[i] as T)) {
+            firstArray = array[i] && array[i] as T
+         }
+      }
+      return firstArray;
+   }
+
+   
+}
